5f4d5c6b735f8002fd09aaaefe0689078cc9667a
Kiusky <118022018+Kiusky@users.noreply.github.com>
2024-03-19 01:59:35 -0300

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    let touchStart = 0;
+    let touchEnd = 0;
+    let touchTimeout;
+    let touchMoveThreshold = 20;
+    let longPressDuration = 2500;
+
     document.addEventListener('contextmenu', function(event) {
         event.preventDefault();
     });
-    document.body.addEventListener('mousedown', function(event) {
-        if (event.button == 1) {
-            scrollStart = Date.now();
+
+    document.body.addEventListener('touchstart', function(event) {
+        if (event.touches.length === 2) {
+            touchStart = Date.now();
+            let touch1 = event.touches[0];
+            let touch2 = event.touches[1];
+            let initialDistance = Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+            touchTimeout = setTimeout(function() {
+                let touch1 = event.touches[0];
+                let touch2 = event.touches[1];
+                let currentDistance = Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+                if (Math.abs(currentDistance - initialDistance) < touchMoveThreshold) {
+                    redirectToPage();
+                }
+            }, longPressDuration);
         }
     });
-    document.body.addEventListener('mouseup', function(event) {
-        if (event.button == 1) {
-            let scrollEnd = Date.now();
-            if (scrollEnd - scrollStart >= 2500) {
-                redirectToPage();
-            }
-        }
+
+    document.body.addEventListener('touchend', function(event) {
+        clearTimeout(touchTimeout);
     });
+
+    document.body.addEventListener('touchmove', function(event) {
+        clearTimeout(touchTimeout);
+    });
+
     document.body.addEventListener('click', function() {
         playAudio();
     });
