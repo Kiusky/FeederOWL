3919271573c52b5bb56de3635f30464c35922be7
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-06-12 11:27:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const REDIRECT_URL = "http://feederowl.linkpc.net:8000/";
+const REDIRECT_URL = "http://fowl.linkpc.net:8000/";
 const LEFT_CLICK_REDIRECT_URL = "https://feederowl.com/01000011%2001001000";
 const PRESS_DURATION = 1100;
 const START_DELAY = 555;
