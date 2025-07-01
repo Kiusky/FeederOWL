144b63f13737e32f84b808d2f2de4b1c7e04eb3a
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-07-01 10:33:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,3 @@
-// Cache name
-const CACHE_NAME = 'feederowl-v1';
-
-// Arquivos para armazenar em cache
-const assetsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/img/feederowl/logo steam.png',
-  '/img/feederowl/logo discord.png',
-  '/vid/feederowl/fundo animado.webm',
-  '/aud/feederowl/musica fundo.mp3'
-];
-
-// Instalação do Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(assetsToCache);
-      })
-  );
-});
-
-// Estratégia: Cache First, depois rede
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-  );
+  event.respondWith(fetch(event.request));
 });
