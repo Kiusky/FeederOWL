4aa0d123e21271354fb0df61bbc60c0c6281c916
Kiusky <118022018+Kiusky@users.noreply.github.com>
2025-09-08 22:10:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("feederowl-cache").then(cache =>
+      cache.addAll([ "/", "/index.html", "/styles.css", "/script.js" ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
